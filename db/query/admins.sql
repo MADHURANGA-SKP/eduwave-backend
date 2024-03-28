5996fa1abfda5ddf4e7cf5eebb62f6b70b440402
MADHURANGA-SKP <pasanmadhuranga333@gmail.com>
2024-03-28 09:29:38 +0530

    query assignment fixes

--- db/query/admins.sql
+++ db/query/admins.sql
@@ -1,10 +1,3 @@
--- name: CreateAdmin :one
-INSERT INTO admins(
-    user_name
-) VALUES (
-    $1
-) RETURNING *;
-
 -- name: GetAdmin :one
 SELECT * FROM admins
 WHERE admin_id = $1;
@@ -14,13 +7,6 @@ UPDATE admins
 SET user_name = $2
 WHERE admin_id = $1
 RETURNING *;
-
--- name: ListAdmin :many
-SELECT * FROM admins
-WHERE user_name = $1
-ORDER BY admin_id
-LIMIT $2
-OFFSET $3;
 
 -- name: DeleteAdmin :exec
 DELETE FROM admins
