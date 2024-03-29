d8147f047fc8f173decf0693efe83a141d3fc16d
MADHURANGA-SKP <pasanmadhuranga333@gmail.com>
2024-03-30 03:40:31 +0530

    server issues fixed

---
+++ api/validator.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"eduwave-back-end/util"
+
+	"github.com/go-playground/validator/v10"
+)
+
+
+var validUsername validator.Func = func(fl validator.FieldLevel) bool {
+	if username, ok := fl.Field().Interface().(string); ok {
+		return util.IsSupportedUsername(username) == nil
+	}
+	return false
+}
---
+++ util/username.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	isValidUsername = regexp.MustCompile(`^[a-zA-Z\s]+$`).MatchString
+)
+
+func ValidateString(value string, minLength int, maxLength int) error {
+	n := len(value)
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("must contain from %d-%d characters", minLength, maxLength)
+	}
+	return nil
+}
+
+//IsSupportUsername returns true if the Username is supported 
+func IsSupportedUsername(username string) error {
+	if err := ValidateString(username, 3 ,100); err != nil {
+		return err
+	}
+	if !isValidUsername(username){
+		return fmt.Errorf("must contain lowercase and Uppercase letters, digits, ot underscore")
+	}
+	return nil
+}
